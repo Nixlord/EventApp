54e8bad2ec235a145af71fe4cea3ab6bcbdc9992
Diksha65 <agarwaldiksha6596@gmail.com>
2019-01-15 12:53:22 +0530

    Developing the event list logic

--- app/src/main/java/com/example/overlord/eventapp/model/Event.kt
+++ app/src/main/java/com/example/overlord/eventapp/model/Event.kt
@@ -1,16 +1,12 @@
 package com.example.overlord.eventapp.model
 
-import android.graphics.drawable.Drawable
-import java.sql.Time
-import java.time.LocalDate
-import java.time.LocalTime
 import java.util.*
 
 class Event (
     val name : String = "",
-    val date : LocalDate,
-    val eventTime : LocalTime,
-    val foodTime : LocalTime,
+    val date : Date,
+    val eventTime : String,
+    val foodTime : String,
     val location : String = "",
     val image : Int,
     val message : String? = ""
